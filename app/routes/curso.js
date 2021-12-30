119e099d091ab77e9951e60c22d741ff89c617e1
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2021-12-30 04:03:52 -0300

    Adequação às aulas.

---
+++ app/routes/curso.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+    var controller = app.controllers.curso;
+    app.route('/cursos')
+        .get(controller.listaCursos);
+    app.route('/cursos/:id')
+        .get(controller.obtemCurso)
+        .delete(controller.removeCurso);
+};
